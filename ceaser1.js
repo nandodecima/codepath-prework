de7a673927e7ed0530d7fdad609c0cd7ae27752b
nandodecima <hnandodecima@gmail.com>
2024-11-13 12:25:05 -0800

    Create ceaser1.js

---
+++ ceaser1.js
@@ -0,0 +1,28 @@
+
+
+Q1
+console.log("partyLocation");
+shiftValue
+encryptedMessage
+decryptedMessage
+isPartySafe
+
+Q2
+let partyLocation = "GARDEN"; //string
+let shiftValue = 3; //number
+let encryptedMessage = "Welcome"; //string
+let decryptedMessage = "Zhofrph"; //string
+let isPartySafe = false; // bolean
+
+Q3
+let partyLocation = "GARDEN"; // the location can be changed
+const shiftValue = 3; // the deciefer code cannot be changed
+let encryptedMessage = "Welcome"; // the welcome message can be changed
+let decryptedMessage = "Zhofrph"; // the decrypted message can be changed
+let isPartySafe = true; // the party safety can be compromised
+
+Q4
+
+const shiftValue = 3;
+Number.isInteger(shiftValue);
+
